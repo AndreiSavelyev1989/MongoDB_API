233cacd1a070b2ca0669a8e4aca41e2dc8f29b71
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2022-12-19 16:04:02 +0300

    added routes POST/PATH

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { connectToDb, getDb } = require("./db");
 
 const PORT = 3000;
 const app = express();
+app.use(express.json());
 
 let db;
 
@@ -57,6 +58,32 @@ app.delete("/movies/:id", (req, res) => {
       .deleteOne({ _id: ObjectId(req.params.id) })
       .then(() => {
         res.status(200).json({ notification: "Item successfully deleted" });
+      })
+      .catch(() => {
+        handleError(res, "Something went wrong...");
+      });
+  } else {
+    handleError(res, "Wrong id");
+  }
+});
+
+app.post("/movies", (req, res) => {
+  db.collection("movies")
+    .insertOne(req.body)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch(() => {
+      handleError(res, "Something went wrong...");
+    });
+});
+
+app.patch("/movies/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("movies")
+      .updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body })
+      .then((result) => {
+        res.status(200).json(result);
       })
       .catch(() => {
         handleError(res, "Something went wrong...");
